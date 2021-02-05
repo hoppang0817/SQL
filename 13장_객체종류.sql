2512b71827ee6503526551a63ee58dab81982e48
hoppang0817 <62546979+hoppang0817@users.noreply.github.com>
2021-02-05 16:25:10 +0900

    20210205

---
+++ 13장_객체종류.sql
@@ -0,0 +1,107 @@
+--13장
+
+--13-1 
+SELECT * FROM DICT;
+
+--13-3 계정이 가지고있는 테이블이름을 알고싶을때유용
+SELECT
+  TABLE_NAME  
+FROM USER_TABLES;
+
+--13-7
+SELECT * FROM DBA_USERS WHERE USERNAME = 'SCOTT';
+
+
+
+
+
+--인덱스 INDEX
+
+--13-8 현재 계정이 소유한 인덱스 정보 확인
+SELECT * FROM USER_INDEXES;
+
+--인덱스 생성
+CREATE INDEX IDX_EMP_SAL
+ON EMP(SAL); --EMP테이블의 SAL열
+
+
+
+--인덱스 확인하기
+SELECT 
+
+      TABLE_NAME     테이블명,
+      INDEX_NAME   인덱스이름,
+      COLUMN_NAME   컬럼이름
+FROM ALL_IND_COLUMNS 
+WHERE TABLE_NAME = 'MEMBERS';
+
+
+----
+CREATE TABLE members(
+    member_id INT,
+    first_name VARCHAR2(100) NOT NULL,
+    last_name VARCHAR2(100) NOT NULL,
+    gender CHAR(1) NOT NULL,
+    dob DATE NOT NULL,
+    email VARCHAR2(255) NOT NULL,
+    PRIMARY KEY(member_id)--기본키 설정
+);
+SELECT * FROM MEMBERS;
+
+
+SELECT * FROM members
+WHERE last_name = 'Harse';
+
+--아래 셀렉트문을 설명문으로 만듬
+EXPLAIN PLAN FOR
+SELECT * FROM members
+WHERE last_name = 'Harse';
+
+SELECT 
+    PLAN_TABLE_OUTPUT 
+FROM 
+    TABLE(DBMS_XPLAN.DISPLAY());
+    
+    
+    
+--인덱스 만들기
+CREATE INDEX MEMBERS_LAST_NAME_I
+ON MEMBERS(LAST_NAME);
+
+
+EXPLAIN PLAN FOR
+SELECT * FROM members
+WHERE last_name = 'Harse';
+
+SELECT 
+    PLAN_TABLE_OUTPUT 
+FROM 
+    TABLE(DBMS_XPLAN.DISPLAY());
+    
+    
+--인덱스 삭제
+DROP INDEX MEMBERS_LAST_NAME_I;
+
+
+
+
+--뷰 VIEW
+
+--13-15 뷰생성
+CREATE VIEW VW_EMP20
+ AS (SELECT EMPNO,ENAME,JOB,DEPTNO FROM EMP WHERE DEPTNO=20);
+
+--가상테이블이지만 실제테이블처럼 사용가능
+SELECT * FROM VW_EMP20; 
+
+--복습
+CREATE VIEW VM_EMP30ALL
+ AS(SELECT * FROM EMP WHERE DEPTNO = 30);
+
+--뷰삭제 : 뷰를 삭제해도 실제 테이블은 삭제 되지않는다.
+DROP VIEW VW_EMP20;
+
+
+--뷰생성 OR REPLACE 사용
+CREATE OR REPLACE VIEW VW_EMP
+ AS (SELECT EMPNO,ENAME,JOB,DEPTNO FROM EMP WHERE DEPTNO=10);
